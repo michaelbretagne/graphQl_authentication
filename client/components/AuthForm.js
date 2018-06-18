02e9946ba955dde3e38bfd9f067fbbc5571d3c72
Michael <michael.bretagne@gmail.com>
2018-06-17 22:22:19 -0700

    Add signin/login errors handler

---
+++ client/components/AuthForm.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+class AuthForm extends Component {
+  state = { email: "", password: "" };
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    this.props.onSubmit(this.state);
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form onSubmit={this.onSubmit.bind(this)} className="col s6">
+          <div className="input-field">
+            <input
+              placeholder="Email"
+              value={this.state.email}
+              onChange={event => this.setState({ email: event.target.value })}
+            />
+          </div>
+          <div className="input-field">
+            <input
+              placeholder="Password"
+              type="password"
+              value={this.state.password}
+              onChange={event =>
+                this.setState({ password: event.target.value })
+              }
+            />
+          </div>
+          <div className="errors">
+            {this.props.errors.map(error => <div key={error}>{error}</div>)}
+          </div>
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AuthForm;
--- client/components/Header.js
+++ client/components/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
       return (
         <div>
           <li>
-            <Link to="/singnup">Signup</Link>
+            <Link to="/signup">Signup</Link>
           </li>
           <li>
             <Link to="/login">Login</Link>
